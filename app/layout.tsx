5bb7d02f278c37d8d44bebc536579e56b1a6b86c
KeyHelper Team <keyhelperteam@gmail.com>
2025-05-05 15:33:16 +0800

    feat: initial project

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from 'next/font/google';
+import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const font = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "KeyHelper App",
+  description: "KeyHelper App Description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${font.className} antialiased`}
+      >
+        <ResponsiveNav />
+        {children}
+      </body>
+    </html>
+  );
+}
